c5ad6cc927f8d77e54bb7b55e18a8e84ead6291d
Olivier Desenfans <olivier@moonsonglabs.com>
2024-01-23 12:50:08 +0100

    OS PIE itest

--- test-cases/cases/starknet-os/generate_proof.sh
+++ test-cases/cases/starknet-os/generate_proof.sh
@@ -2,5 +2,5 @@ cpu_air_prover \
   --out_file output/proof.json \
   --public_input_file output/air_public_input.json \
   --private_input_file output/air_private_input.json \
-  --prover_config_file output/cpu_air_prover_config.json \
-  --parameter_file output/cpu_air_params.json
+  --prover_config_file cpu_air_prover_config.json \
+  --parameter_file cpu_air_params.json
