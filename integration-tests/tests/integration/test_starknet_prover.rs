eab2c254147409f874b6ce47fd02ed8a8b47e86a
Stephen Shelton <steve@brewcraft.org>
2024-01-24 15:29:43 -0700

    Add integration test to cover splitting proof

--- integration-tests/tests/integration/test_starknet_prover.rs
+++ integration-tests/tests/integration/test_starknet_prover.rs
@@ -33,6 +33,37 @@ mod tests {
 
         let proof = result.unwrap();
         assert_eq!(proof.proof_hex, expected_proof.proof_hex);
+        assert!(proof.split_proofs.is_none());
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_execute_and_prove_and_split(
+        #[future] starknet_prover_client_server: (RpcClient, RpcServer),
+    ) {
+        let test_case_dir = get_test_case_file_path("bootloader/programs/fibonacci");
+        let program_file = test_case_dir.join("program.json");
+        let proof_file = test_case_dir.join("output/proof.json");
+        let program_bytes = std::fs::read(program_file).unwrap();
+        let expected_proof: Proof = read_json_from_file(proof_file).unwrap();
+
+        let (mut client, _server) = starknet_prover_client_server.await;
+
+        let programs = vec![program_bytes];
+        let pies = vec![];
+        let split_proof = true;
+
+        let result = execute_and_prove(&mut client, programs, pies, split_proof).await;
+
+        assert!(result.is_ok(), "{:?}", result);
+
+        let proof = result.unwrap();
+        assert_eq!(proof.proof_hex, expected_proof.proof_hex);
+
+        assert!(proof.split_proofs.is_some());
+        let split_proofs = proof.split_proofs.unwrap();
+        assert!(split_proofs.merkle_statements.len() > 0);
+        assert!(split_proofs.fri_merkle_statements.len() > 0);
     }
 
     #[ignore = "this test takes ~5 minutes to run"]
